e0be0048793451857b4ed12073c6c8feeb584163
franceme <frantzme@vt.edu>
2022-10-07 15:51:27 -0400

    Update ghspace.py

--- ghspace.py
+++ ghspace.py
@@ -8,26 +8,31 @@ def run(string):
     os.system(string)
 
 containerName = "<>"
-if len(sys.argv) >= 2:
+if len(sys.argv) >= 2 and sys.argv[1] != "--ini":
     containerName = sys.argv[1]
 
-for cmd in [
-    "gh auth login",
-    "gh auth refresh -h github.com -s codespace",
-    #"gh codespace ssh",
-
+cmds = [
     "gh codespace ssh --config >> ~/.ssh/config",
     "cat ~/.ssh/codespaces"
-]:
+]
+if "--ini" in sys.argv:
+    cmds = [
+        "gh auth login",
+        "gh auth refresh -h github.com -s codespace",
+        #"gh codespace ssh",
+    ] + cmds
+
+for cmd in cmds:
     run(cmd)
 
-with open("~/.ssh/codespaces","a+") as writer:
-    writer.write(f"""
-Host {containerName}.dev-container
-    User vscode
-    ProxyCommand /usr/local/bin/gh cs ssh -c {containerName} --stdio
-    UserKnownHostsFile=/dev/null
-    StrictHostKeyChecking no
-    LogLevel quiet
-    ControlMaster auto
-""")
+if False:
+    with open("~/.ssh/codespaces","a+") as writer:
+        writer.write(f"""
+    Host {containerName}.dev-container
+        User vscode
+        ProxyCommand /usr/local/bin/gh cs ssh -c {containerName} --stdio
+        UserKnownHostsFile=/dev/null
+        StrictHostKeyChecking no
+        LogLevel quiet
+        ControlMaster auto
+    """)
