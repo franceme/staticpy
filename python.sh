410f0473d4094dcaf20afa80ae2d118d9dca0751
franceme <frantzme@vt.edu>
2022-11-15 10:47:23 -0500

    Update python.sh

--- python.sh
+++ python.sh
@@ -22,11 +22,11 @@ sleep 1m
 
 wget https://bootstrap.pypa.io/get-pip.py
 { # try
-    Python-3.8.15/python get-pip.py &&
+    Python-3.8.15/python get-pip.py
 } || { # catch
 }
 {
-    python get-pip.py &&
+    python get-pip.py
 } || { # catch
     # save log for exception 
 }
