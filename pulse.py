a8f03e510981c4809781e924425365b23aa367b6
franceme <frantzme@vt.edu>
2022-12-23 19:03:50 -0500

    Update pulse.py

--- pulse.py
+++ pulse.py
@@ -1,5 +1,5 @@
 import os,sys
 
 os.system("""
-sudo rpm -i https://secure.nis.vt.edu/resources/downloads/ps-pulse-linux-9.1r15.0-b15819-64bit-installer.rpm
+sudo dpkg -i https://secure.nis.vt.edu/resources/downloads/ps-pulse-linux-9.1r15.0-b15819-64bit-installer.deb
 """.strip())
