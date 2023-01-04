3a38f3251282586fef9c77ee124f0a4cb9b114da
franceme <frantzme@vt.edu>
2023-01-04 18:14:09 -0500

    Update rshell.sh

--- rshell.sh
+++ rshell.sh
@@ -4,4 +4,4 @@ git clone https://gitlab.redox-os.org/redox-os/ion/
 cd ion
 RUSTUP=0 make # By default RUSTUP equals 1, which is for developmental purposes
 sudo make install prefix=/usr
-sudo make update-shells prefix=/usr
+#sudo make update-shells prefix=/usr
