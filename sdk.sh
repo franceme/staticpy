b2d7dfb468123ea114f8c2660b13ac866032317a
franceme <frantzme@vt.edu>
2024-01-22 15:56:24 -0500

    patch 11 on

--- sdk.sh
+++ sdk.sh
@@ -43,12 +43,27 @@ function install_eight {
     yes|sdk i java $VAR
 }
 
+function install_leven {
+    sourcing
+
+    IS_INSTALLED=$(sdk ls java|grep zulu|grep '11.0'|head -n 1)
+    HAS_INSTALLED='installed'
+
+    if echo "$IS_INSTALLED" | grep -q -E "$HAS_INSTALLED"; then
+        echo "Java JDK Version already installed";
+        exit 0
+    fi
+
+    VAR=$(sdk ls java|grep zulu|grep '11.0'|head -n 1|awk -F ' ' '{print $8}')
+    yes|sdk i java $VAR
+}
+
 
 # https://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-9.html#ss9.1
 
-OPTIONS="all eight seven"
+OPTIONS="all eight seven eleven"
 if [ -z "$1" ]; then 
-    echo usage: $0 "all | eight | seven"
+    echo usage: $0 "all | eight | seven | eleven"
     exit
 fi
 
@@ -63,6 +78,10 @@ fi
 
 if [ $1 == "eight" ] || [ $1 == "all" ]; then
 echo "Installing java jdk 8" && install_eight
+fi
+
+if [ $1 == "eleven" ] || [ $1 == "all" ]; then
+echo "Installing java jdk 11" && install_leven
 fi
 
 exit 0
