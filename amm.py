a23ea3dc69b444f1089482adfca3f7250858c1bc
franceme <frantzme@vt.edu>
2024-03-18 21:28:28 -0400

    added amm

---
+++ amm.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import os,sys
+
+#From :> https://ammonite.io/#Reference
+
+cmd = """sudo sh -c '(echo "#!/usr/bin/env sh" && curl -L https://github.com/com-lihaoyi/Ammonite/releases/download/3.0.0-M1/2.12-3.0.0-M1) > /usr/local/bin/amm && chmod +x /usr/local/bin/amm' && amm"""
+
+print(cmd);os.system(cmd)
