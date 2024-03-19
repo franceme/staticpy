f8d642692297fa1012624be786255ab2a983b87c
franceme <frantzme@vt.edu>
2024-03-18 21:50:54 -0400

    re

--- amm.py
+++ amm.py
@@ -6,4 +6,11 @@ import os,sys
 
 cmd = """sudo sh -c '(echo "#!/usr/bin/env sh" && curl -L https://github.com/com-lihaoyi/Ammonite/releases/download/3.0.0-M1/2.12-3.0.0-M1) > /usr/local/bin/amm && chmod +x /usr/local/bin/amm' && amm"""
 
-print(cmd);os.system(cmd)
+print(cmd);
+
+try:os.system(cmd)
+except:pass
+
+
+try:os.system(cmd.replace("sudo ", ""))
+except:pass
