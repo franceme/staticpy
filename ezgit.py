c71d4e99ee509cc7f9bbd91ba54254db83b3c26b
franceme <frantzme@vt.edu>
2024-04-08 08:23:10 -0400

    added some slight colors

--- ezgit.py
+++ ezgit.py
@@ -5,6 +5,84 @@ def run(string):
 	print(string)
 	os.system(string)
 
+
+class Color(object):
+	"""
+	reference from https://gist.github.com/Jossef/0ee20314577925b4027f and modified bit.
+	"""
+
+	def __init__(self, text, **user_styles):
+
+		styles = {
+			# styles
+			'reset': '\033[0m',
+			'bold': '\033[01m',
+			'disabled': '\033[02m',
+			'underline': '\033[04m',
+			'reverse': '\033[07m',
+			'strike_through': '\033[09m',
+			'invisible': '\033[08m',
+			# text colors
+			'fg_black': '\033[30m',
+			'fg_red': '\033[31m',
+			'fg_green': '\033[32m',
+			'fg_orange': '\033[33m',
+			'fg_blue': '\033[34m',
+			'fg_purple': '\033[35m',
+			'fg_cyan': '\033[36m',
+			'fg_light_grey': '\033[37m',
+			'fg_dark_grey': '\033[90m',
+			'fg_light_red': '\033[91m',
+			'fg_light_green': '\033[92m',
+			'fg_yellow': '\033[93m',
+			'fg_light_blue': '\033[94m',
+			'fg_pink': '\033[95m',
+			'fg_light_cyan': '\033[96m',
+			'fg_white': '\033[97m',
+			'fg_default': '\033[99m',
+			# background colors
+			'bg_black': '\033[40m',
+			'bg_red': '\033[41m',
+			'bg_green': '\033[42m',
+			'bg_orange': '\033[43m',
+			'bg_blue': '\033[44m',
+			'bg_purple': '\033[45m',
+			'bg_cyan': '\033[46m',
+			'bg_light_grey': '\033[47m'
+		}
+
+		self.color_text = ''
+		for style in user_styles:
+			try:
+				self.color_text += styles[style]
+			except KeyError:
+				raise KeyError('def color: parameter `{}` does not exist'.format(style))
+
+		self.color_text += text
+
+	def __format__(self):
+		return '\033[0m{}\033[0m'.format(self.color_text)
+
+	@classmethod
+	def red(clazz, text):
+		cls = clazz(text, bold=True, fg_red=True)
+		return cls.__format__()
+
+	@classmethod
+	def orange(clazz, text):
+		cls = clazz(text, bold=True, fg_orange=True)
+		return cls.__format__()
+
+	@classmethod
+	def green(clazz, text):
+		cls = clazz(text, bold=True, fg_green=True)
+		return cls.__format__()
+
+	@classmethod
+	def custom(clazz, text, **custom_styles):
+		cls = clazz(text, **custom_styles)
+		return cls.__format__()
+
 def exec(string, display=True, lines=False):
 	import subprocess
 
@@ -105,6 +183,7 @@ if __name__ == '__main__':
 			import mystring
 
 		for foil_line in mystring.string("git status").exec(display=False,lines=True):
+			line_to_display = None
 			og_foil_loine = dc(foil_line)
 			foil_line = foil_line.strip()
 			if foil_line.startswith("modified"):
@@ -113,9 +192,15 @@ if __name__ == '__main__':
 				if len(foil_diff) == 3 and foil_diff[1].startswith("old mode") and foil_diff[2].startswith("new mode"):
 					pass
 				else:
-					print(og_foil_loine, end='\n',flush=True)
+					line_to_display = og_foil_loine
 			else:
-				print(og_foil_loine, end='\n',flush=True)
+				line_to_display = og_foil_loine
+
+			if line_to_display:
+				if "/" in line_to_display and "." in line_to_display:
+					print(Color.custom(og_foil_loine, bold=True, fg_red=True), end='\n',flush=True)
+				else:
+					print(og_foil_loine, end='\n',flush=True)
 	if args.backward:
 		run("git reset --soft HEAD~1")
 	if args.init:
