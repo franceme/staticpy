af8c7a3bc532a4d8a506fd8a6f434991f13e7084
franceme <frantzme@vt.edu>
2024-05-27 18:44:15 -0400

    re

--- bal.py
+++ bal.py
@@ -7,6 +7,7 @@ def getArgs():
 	parser = argparse.ArgumentParser("Ballerina Aide")
 	parser.add_argument("--localize", help="Clone, build, localize, and remove a custom repository (since ballerina central has some problems).",nargs=1, default=None)
 	parser.add_argument("--user", help="Username of github user.",nargs=1, default="franceme")
+	parser.add_argument("--save", help="Don't delete the repository",action="store_true",default=False)
 	args,unknown = parser.parse_known_args()
 	return args
 
@@ -17,13 +18,20 @@ if __name__ == "__main__":
 	#https://ballerina.io/learn/manage-dependencies/
 	if args.localize:
 		localize = args.localize[0]
-		for x in [
-			'git clone git@github.com:{0}/{1}'.format(args.user[0], localize),
-			'cd {0} && bal pack'.format(localize),
-			'cd {0} && bal push --repository local'.format(localize),
-			'yes|rm -r {0}'.format(localize),
-		]:
-			print(x);os.system(x);
+		cmds = []
+
+		if not os.path.exists(localize):
+			cmds += ['git clone git@github.com:{0}/{1}'.format(args.user[0], localize)]
+
+		cmds += ['cd {0} && bal pack'.format(localize)]
+		cmds += ['cd {0} && bal push --repository local'.format(localize)]
+
+		if not args.save:
+			cmds += ['yes|rm -r {0}'.format(localize)]
+
+		for x in cmds:
+			print(x);os.system(x)
+
 		with open("Ballerina.toml", "a+") as writer:
 			for line in [
 				" ",
