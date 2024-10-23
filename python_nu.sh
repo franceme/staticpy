0583236eab59c02790d11bae3d505371f68edd9a
franceme <frantzme@vt.edu>
2024-10-22 22:02:09 -0400

    added python3.10

---
+++ python_nu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Installing the Dependencies"
+
+#Ubuntu
+yes|apt install wget curl zlib1g-dev libreadline-gplv2-dev libncurses5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev
+
+#Debian Link
+#https://linuxize.com/post/how-to-install-python-3-8-on-debian-10/
+
+echo "Downloading Python3"
+wget https://www.python.org/ftp/python/3.10.15/Python-3.10.15.tar.xz
+
+echo "Extracting the zip"
+tar -xvf Python-3.10.15.tar.xz
+cd Python-3.10.15 && ./configure && make && make install
+
+sleep 1m
+
+echo "Getting PIP"
+sleep 1m
+
+wget https://bootstrap.pypa.io/get-pip.py
+#Python-3.10.15/python get-pip.py
